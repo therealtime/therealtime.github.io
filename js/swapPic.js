420d05a826085093ce276ddf9fa0300427d6fdbc
TheRealTime <47280665+therealtime@users.noreply.github.com>
2020-04-21 15:17:18 -0700

    fix logo

--- js/swapPic.js
+++ js/swapPic.js
@@ -6,13 +6,13 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
       // downscroll code
       if(window.pageYOffset > 150){
         document.getElementById("logo").class = "img-nav";
-        document.getElementById("logo").src = "./images/accelabit2.png";
+        document.getElementById("logo").src = "./images/accelabit2.PNG";
       }
    } else {
       // upscroll code
       if(window.pageYOffset < 150){
         document.getElementById("logo").class = "img-nav";
-        document.getElementById("logo").src = "./images/accelabit.png";
+        document.getElementById("logo").src = "./images/accelabit.PNG";
       }
    }
    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
